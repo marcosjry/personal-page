7349a0244730b5085517ad91804e11870f1158aa
Marcos Antônio <marcosjunior.me.009@gmail.com>
2024-05-04 15:26:18 -0300

    feat: criando service da requisição do repositório

---
+++ src/app/services/github.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import axios from 'axios';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+
+  constructor() { }
+
+  getRepos(username: string) {
+    return axios.get(`https://api.github.com/users/${username}/repos`);
+  }
+}
